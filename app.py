b8a60366b5b7765a8314d8d20987224df81fa0de
HanuHoney <hanu.nagineni@gmail.com>
2026-04-23 12:21:47 +0530

    Updated python app to run only when the CI is trigerred

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ class Handler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Hello from Hanu Priya DevOps!")
 
-HTTPServer(("", 8080), Handler).serve_forever()
+if __name__ == "__main__":
+    HTTPServer(("", 8080), Handler).serve_forever()
